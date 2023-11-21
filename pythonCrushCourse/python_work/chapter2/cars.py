a3b97a330aab3282505c912b9b6c1e9b4c223282
GakuruUri <urigakuru@live.co.uk>
2023-11-21 19:50:22 +0300

    Day 32_1

--- pythonCrushCourse/python_work/chapter2/cars.py
+++ pythonCrushCourse/python_work/chapter2/cars.py
@@ -1,7 +1,16 @@
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 
+print("Here is the original list:")
+print(cars)
 
+print("\nHere is the sorted list:")
 print(sorted(cars))
+
+cars.reverse()
+print(cars)
+
+print("\nHere is the original list again:")
+print(cars)
 
 # Sorting a list gives parmanet changes
 cars.sort()
