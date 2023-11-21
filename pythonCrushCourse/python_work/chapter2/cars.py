818d36b22466b34fec0159012ce844879182c975
GakuruUri <urigakuru@live.co.uk>
2023-11-21 19:21:01 +0300

    Day 38

---
+++ pythonCrushCourse/python_work/chapter2/cars.py
@@ -0,0 +1,10 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+
+# Sorting a list gives parmanet changes
+cars.sort()
+print(cars)
+
+# This also results in parmanet changes.
+cars.sort(reverse=True)
+print(cars)
