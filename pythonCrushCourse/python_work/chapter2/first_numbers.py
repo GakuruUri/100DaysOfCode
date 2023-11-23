d233c4e1dd4fc9d5784a67b2766dfb269fa7de0d
GakuruUri <urigakuru@live.co.uk>
2023-11-23 21:46:11 +0300

    Day 39

---
+++ pythonCrushCourse/python_work/chapter2/first_numbers.py
@@ -0,0 +1,45 @@
+for value in range(1, 5):
+    print(value)
+    
+    
+numbers = list(range(1, 6))
+print(numbers)
+
+
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+
+
+
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append(square)
+    
+print(squares)
+
+# Counting to 20 and all odds upto 20
+twentys = []
+for value in range(1, 21, 2):
+    twentys.append(value)
+print(twentys)
+
+# Counting 1 to a million
+num_milli = list(range(1000001))
+print(sum(num_milli))
+print(min(num_milli))
+print(max(num_milli))
+print(num_milli)
+
+# Multiples of 3
+threes = list(range(3, 31, 3))
+print(threes)
+
+
+
+cubes = []
+for value in range(1, 11):
+    cube = value ** 3
+    cubes.append(cube)
+    
+print(cubes)
---
+++ pythonCrushCourse/python_work/chapter2/pizza.py
@@ -0,0 +1,17 @@
+pizzas = ["margarita", "chicken", "beef"]
+for pizza in pizzas:
+    # print(pizza)
+    print("\n================================")
+    print(f"I really like {pizza.title()} pizza")
+    
+    
+print("\n================================")
+print("Gah damn it, i really really love pizza!\n")
+
+
+jungle_cats = ["lion", "tiger", "leopard"]
+for cat in jungle_cats:
+    print(f" A {cat.title()}, would be a bloody great pet\n")
+    
+    
+print("All these cats would also turn and fuckin' eat you")
