8af08a6d4902677fcd83954562575f99a0494956
GakuruUri <urigakuru@live.co.uk>
2023-11-29 20:54:36 +0300

    Day 9

--- pythonCrushCourse/python_work/Part 1/fovourite_fruits.py
+++ pythonCrushCourse/python_work/Part 1/fovourite_fruits.py
@@ -13,3 +13,16 @@ if 'dragon fruit' in favourite_fruits:
 if 'cassava fruit' in favourite_fruits:
     print("You really like cassava fruit")
 print("\nWhat a list of fruits!")
+
+# Make a list of your three favorite fruits
+favorite_fruits = ['apple', 'banana', 'cherry']
+
+# Write five if statements
+fruits = ['apple', 'grape', 'banana', 'watermelon', 'cherry']
+
+for fruit in fruits:
+    if fruit in favorite_fruits:
+        print(f"I really like {fruit}!")
+    else:
+        print(f"{fruit} is not in my favorite fruits list.")
+
