7ab0243284b3d0404d11a29ad7affdb52791686b
GakuruUri <urigakuru@live.co.uk>
2023-11-29 10:33:35 +0300

    Day 9

---
+++ pythonCrushCourse/python_work/Part 1/fovourite_fruits.py
@@ -0,0 +1,15 @@
+favourite_fruits = ['Banana', 'Pineapple', 'Apple', 'Mango']
+
+if 'Banana' in favourite_fruits:
+    print("You really like Banana")
+if 'Pineapple' in favourite_fruits:
+    print("You really like Pineapple")
+if 'Apple' in favourite_fruits:
+    print("You really like Apple")
+if 'Mango' in favourite_fruits:
+    print("You really like Mango")
+if 'dragon fruit' in favourite_fruits:
+    print("You really like dragon fruit")
+if 'cassava fruit' in favourite_fruits:
+    print("You really like cassava fruit")
+print("\nWhat a list of fruits!")
