3e486beeda70f381ca448a1bd39b47744e26ce83
GakuruUri <urigakuru@live.co.uk>
2023-12-03 17:35:45 +0300

    Day 14 dict update

--- pythonCrushCourse/python_work/Part 1/alien.py
+++ pythonCrushCourse/python_work/Part 1/alien.py
@@ -10,13 +10,13 @@ new_points = alien_0["points"]
 print(f"\nYou just earned {new_points} points!")
 
 # Adding new key-value pairs
-alien_0 = {'color': 'green', 'points': 5}
+alien_0 = {"color": "green", "points": 5}
 
 print(alien_0)
 
 
-alien_0['x-position'] = 0
-alien_0['y-position'] = 25
+alien_0["x-position"] = 0
+alien_0["y-position"] = 25
 print(alien_0)
 
 
@@ -25,10 +25,70 @@ print(alien_0)
 print("\n")
 alien_0 = {}
 
-alien_0['color'] = 'green'
-alien_0['points'] = 5
+alien_0["color"] = "green"
+alien_0["points"] = 5
 
 print(alien_0)
 
 # Modifying values in a dictionary
+alien_0 = {"color": "green"}
+print(f"\nThe alien is {alien_0['color']}.")
+
+alien_0["color"] = "yellow"
+print(f"The alien is now {alien_0['color']}.")
+
+
+# Tracking the position of the alien at different speeds.
+alien_0 = {"x_position": 0, "y_position": 25, "speed": "medium", "color": "green"}
+print(f"Original position: {alien_0['x_position']} and the color is {alien_0['color']}")
+
+
+# Move the alien to the right.
+# Determine how far to move the alien based on its current speed.
+
+if alien_0["speed"] == "slow":
+    x_increment = 1
+elif alien_0["speed"] == "medium":
+    x_increment = 2
+else:
+    # This must be a fast alien.
+    x_increment = 3
+alien_0["color"] = "yellow"
+
+# The new position is the old position plus the increment
+alien_0["x_position"] = alien_0["x_position"] + x_increment
+print(f"New position: {alien_0['x_position']} and alien_0 color is {alien_0['color']}")
+
+
+# Removing Key_Value pairs
+alien_0 = {'color': 'green', 'points': 5}
+print("\n")
+print(alien_0)
+
+del alien_0['points']
+print(alien_0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ pythonCrushCourse/python_work/Part 1/favorite_languages.py
@@ -0,0 +1,25 @@
+# Dictionaries of similar object
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+}
+
+
+# Lookup favorite language for a person who took the poll
+
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+}
+
+
+language = favorite_languages['sarah'].title()
+print(f"\nSarah's favorite languages is {language}.")
+
+
