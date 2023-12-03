50082a3a8d7db711c5b73891714c1549c59f4bfd
GakuruUri <urigakuru@live.co.uk>
2023-12-03 12:07:03 +0300

    Day 14

--- pythonCrushCourse/python_work/Part 1/alien.py
+++ pythonCrushCourse/python_work/Part 1/alien.py
@@ -1,11 +1,25 @@
+alien_0 = {"color": "green", "points": 5}
+
+print(alien_0["color"])
+print(alien_0["points"])
+
+# Day 13
+alien_0 = {"color": "green", "points": 5}
+
+new_points = alien_0["points"]
+print(f"\nYou just earned {new_points} points!")
+
+# Adding new key-value pairs
 alien_0 = {'color': 'green', 'points': 5}
 
-print(alien_0['color'])
-print(alien_0['points'])
+print(alien_0)
 
 
+alien_0['x-position'] = 0
+alien_0['y-position'] = 25
+print(alien_0)
 
-alien_0 = {'color':'green', 'points': 5}
 
-new_points = alien_0['points']
-print(f"You just earned {new_points} points!")
+# Starting with an empty dictionary
+
+
