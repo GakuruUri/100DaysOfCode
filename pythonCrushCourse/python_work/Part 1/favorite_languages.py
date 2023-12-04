f6088a7f0dc43151bb6b35a69f82c46c7ab119eb
GakuruUri <urigakuru@live.co.uk>
2023-12-04 20:46:50 +0300

    Day 15

--- pythonCrushCourse/python_work/Part 1/favorite_languages.py
+++ pythonCrushCourse/python_work/Part 1/favorite_languages.py
@@ -36,4 +36,54 @@ for name, language in favorite_languages.items():
 
 
 
+# Looping through all Keys in a dictionary.
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+ }
+
+for name in favorite_languages.keys():
+    print(name.title())
+    
+    
+# Also below applies as we are looping through the keys which is the default
+for name in favorite_languages:
+    print(name.title())
+
+print("\n")
+
+
+# Accessing values associated with certain keys in a loop
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+}
+
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}.")
+    
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+
+
+# Using the keys() method to check if a value/key exists.
+
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+}
+
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
 
