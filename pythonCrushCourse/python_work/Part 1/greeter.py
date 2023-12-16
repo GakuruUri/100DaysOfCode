92c23acb1f27c40acf4a6f4efa7853a61a5b2c5d
Uri Gakuru Karanja <urigakuru@icloud.com>
2023-12-16 06:52:44 +0000

    Day 22

--- pythonCrushCourse/python_work/Part 1/greeter.py
+++ pythonCrushCourse/python_work/Part 1/greeter.py
@@ -1,13 +1,30 @@
-name = input("Please enter your name: ")
+# name = input("Please enter your name: ")
 
-print(f"\nHello, {name}!")
-
-
-prompt = "If you share your name, we can personalize the message you see."
-prompt += "\nwhat is your first name? "
+# print(f"\nHello, {name}!")
 
 
-name = input(prompt)
-print(f"\nHello, {name}!")
+# prompt = "If you share your name, we can personalize the message you see."
+# prompt += "\nwhat is your first name? "
+
+
+# name = input(prompt)
+# print(f"\nHello, {name}!")
+
+# Defining a finction.
+
+def greet_user():
+    """Display a simple greeting."""
+    print("Hello!")
+
+greet_user()
+
+
+# Passing information to a function
+print("\n===Pass info to a function.===")
+def greet_user(username):
+    # Display a simmple greeting
+    print(f"Hello, {username.title()}!")
+
+greet_user('Gakuru')
 
 
