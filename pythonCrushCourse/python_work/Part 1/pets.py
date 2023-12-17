cf08ef8d33c83b9a5330b30e73394baf6e7cc97c
GakuruUri <urigakuru@live.co.uk>
2023-12-17 14:00:45 +0300

    Day 23

--- pythonCrushCourse/python_work/Part 1/pets.py
+++ pythonCrushCourse/python_work/Part 1/pets.py
@@ -14,3 +14,15 @@ def describe_pet(animal_type, pet_name):
 describe_pet('hamster', 'harry')
 describe_pet('dog', 'willie')
 
+# Describe describe_pet in pets.py using keyword arguments
+print("\n====Using keyword arguments =====   ")
+def describe_pet(animal_type, pet_name):
+    """Display infromation about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+    
+describe_pet(animal_type='hamster', pet_name='harry')
+describe_pet(pet_name='willie', animal_type='dog')
+describe_pet(pet_name='harry', animal_type='hamster')
+
+
