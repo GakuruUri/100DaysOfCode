20b6fbf58647a0189bd00a84ccf8434d60c5c29c
GakuruUri <urigakuru@icloud.com>
2024-01-11 19:38:49 +0300

    Been a day

--- pythonCrushCourse/python_work/Part 1/greet_user.py
+++ pythonCrushCourse/python_work/Part 1/greet_user.py
@@ -1,5 +1,5 @@
 def greet_users(names):
-    """Print a simple greeting to each user in a list."""
+    """Print a simple greeting to each user in a list..."""
     for name in names:
         message = f"Hello, {name.title()}!"
         print(message)
