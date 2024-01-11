c4dec33d1de49bec51e77611320fe240cceefdbc
GakuruUri <urigakuru@icloud.com>
2024-01-11 22:21:32 +0300

    Day 29

--- pythonCrushCourse/python_work/Part 1/printing_models.py
+++ pythonCrushCourse/python_work/Part 1/printing_models.py
@@ -1,97 +1,51 @@
-'''
 # Start with some designs that need to be printed.
 unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
 completed_models = []
 
 
-# Simulate printing each design until none is left
-# Move each design to completed modelsafter printing
+# Simulate printing each model, until none are left.
+# Move each design to completed_models after printing.
 
 while unprinted_designs:
     current_design = unprinted_designs.pop()
-    print(f"Printing model now...: {current_design}")
+    print(f"Printing model: {current_design}")
     completed_models.append(current_design)
-   
-    
+
+
 # Display all completed models.
 print("\nThe following models have been printed:")
 for completed_model in completed_models:
     print(completed_model)
-    
-    
-    
-'''    
-    
-    
-# Extend the above using 2 functions for efficiency
+
+""" 
+We can reorganize this code by writing two functions, each of which
+does one specific job. Most of the code won’t change; we’re just structuring
+it more carefully. The first function will handle printing the designs, and
+the second will summarize the prints that have been made:
+"""
 
 def print_models(unprinted_designs, completed_models):
     """
-    Simulate printing each design, until none are left.
+    Simullate printing each design, until none are left.
     Move each design to completed_models after printing.
     """
-    
     while unprinted_designs:
         current_design = unprinted_designs.pop()
-        print(f"Printing model: {current_design}")
+        print(f"Printing models: {current_design}")
         completed_models.append(current_design)
-        
-def show_completed_models(commpleted_models):
-    """Show all the models that were printed."""
+
+
+def show_completed_models(completed_models):
+    """Show all models that were printed."""
     print("\nThe following models have been printed:")
     for completed_model in completed_models:
         print(completed_model)
-        
-        
+
 unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
 completed_models = []
-
 
 print_models(unprinted_designs, completed_models)
 show_completed_models(completed_models)
-print(unprinted_designs)
-print(completed_models)
-    
-    
-    
-# Preventing a function from modifying a list. you use a slice [:]
-def print_models(unprinted_designs, completed_models):
-    """
-    Simulate printing each design, until nonr are left.
-    Move each design to completed_models after printing
-    """    
-    while unprinted_designs:
-        current_design = unprinted_designs.pop()
-        print(f"Printing model: {current_design}")
-        completed_models.append(current_design)
-        
-def show_completed_models(completed_models):
-    """Show all the models that were printed."""
-    print("\nThe following models have been printed:")
-    for completed_model in completed_models:
-        print(completed_model)
-        
-        
-unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
-completed_models = []
 
-print_models(unprinted_designs[:], completed_models)
-show_completed_models(completed_models)
 
-print(unprinted_designs)
-print(completed_models)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
