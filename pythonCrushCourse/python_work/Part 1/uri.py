87fafc0888048ed651d28b70d176346e774f91d8
GakuruUri <urigakuru@icloud.com>
2024-01-13 23:20:53 +0300

    Day 30

--- pythonCrushCourse/python_work/Part 1/uri.py
+++ pythonCrushCourse/python_work/Part 1/uri.py
@@ -1,4 +1,84 @@
-def sum(a, b):
-    return a + b
 
-sum(1,2)
+"""
+def show_message(messages):
+    for message in messages:
+        print(message)      
+
+messages = ['Hi', 'There you', 'Okay!']
+show_message(messages)
+
+"""
+
+
+def show_messages(messages):
+    # Print Text messages
+    for message in messages:
+        print(message)
+
+def send_messages(messages, sent_messages):
+    # Print messages and move them to a list.
+    while messages:
+        message = messages.pop()
+        print(message)
+        sent_messages.append(message)
+        
+        
+
+messages = ['Hi', 'There you', 'Okay!']
+sent_messages = []
+
+
+send_messages(messages[:], sent_messages)
+print(messages)
+print(sent_messages)
+
+
+
+'''
+# Slicing
+
+def show_messages(messages):
+    # Print text messages
+    for message in messages:
+        print(message)
+        
+        
+def send_messages(message, sent_messages):
+    # Print messages and move them to list
+    while messages:
+        message = messages.pop()
+        print(message)
+        sent_messages.append(message)
+        
+messages = ['Hi', 'There you', 'Okay!']
+sent_messages = []
+
+
+send_messages(messages[:], sent_messages)
+print(messages)
+print(sent_messages)
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
