b630d17b6c0fbb363f8edb357516610ba02d3e0f
GakuruUri <urigakuru@icloud.com>
2024-01-15 21:56:20 +0300

    Day 32

--- pythonCrushCourse/python_work/Part 1/pizza.py
+++ pythonCrushCourse/python_work/Part 1/pizza.py
@@ -1,4 +1,4 @@
-
+'''
 pizzas = ["margarita", "chicken", "beef"]
 for pizza in pizzas:
     # print(pizza)
@@ -55,4 +55,105 @@ def make_pizza(size, *toppings):
     print(f"\nMaking a {size}-inche pizza with the following toppings: ")
     for topping in toppings:
         print(f"-{topping}")
+'''
+
+
+"""
+def show_message(messages):
+    for message in messages:
+        print(message)      
+
+messages = ['Hi', 'There you', 'Okay!']
+show_message(messages)
+
+"""
+'''
+
+def show_messages(messages):
+    # Print Text messages
+    for message in messages:
+        print(message)
+
+def send_messages(messages, sent_messages):
+    # Print messages and move them to a list.
+    while messages:
+        message = messages.pop()
+        print(message)
+        sent_messages.append(message)
+        
+        
+
+messages = ['Hi', 'There you', 'Okay!']
+sent_messages = []
+
+
+send_messages(messages[:], sent_messages)
+print(messages)
+print(sent_messages)
+'''
+
+
+'''
+# Slicing
+
+def show_messages(messages):
+    # Print text messages
+    for message in messages:
+        print(message)
+        
+        
+def send_messages(message, sent_messages):
+    # Print messages and move them to list
+    while messages:
+        message = messages.pop()
+        print(message)
+        sent_messages.append(message)
+        
+messages = ['Hi', 'There you', 'Okay!']
+sent_messages = []
+
+
+send_messages(messages[:], sent_messages)
+print(messages)
+print(sent_messages)
+
+'''
+
+'''
+def make_pizza(*toppings):
+    #Print the list of toppings that have been requested.
+    print(toppings)
+    
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
+'''
+
+
+def make_pizza(*toppings):
+    # Summarize the pizza we are about to make.
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+
+# Mixing Position and Arbitrary Arguments
+def make_pizza(size, *toppings):
+    # Summarize the pizza we are about to make.
+    print(f"\nMaking a {size}-inch pizza with the following toppings:")
+
+    for topping in toppings:
+        print(f"-{toppings}")
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+
+
+
+
+
+
 
--- pythonCrushCourse/python_work/Part 1/uri.py
+++ pythonCrushCourse/python_work/Part 1/uri.py
@@ -8,7 +8,7 @@ messages = ['Hi', 'There you', 'Okay!']
 show_message(messages)
 
 """
-
+'''
 
 def show_messages(messages):
     # Print Text messages
@@ -31,7 +31,7 @@ sent_messages = []
 send_messages(messages[:], sent_messages)
 print(messages)
 print(sent_messages)
-
+'''
 
 
 '''
@@ -60,10 +60,35 @@ print(sent_messages)
 
 '''
 
+'''
+def make_pizza(*toppings):
+    #Print the list of toppings that have been requested.
+    print(toppings)
+    
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
+'''
+
+'''
+def make_pizza(*toppings):
+    # Summarize the pizza we are about to make.
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
+'''
 
 
+def build_profile(first, last, **user_info):
+    #Build a dictionary containing everything we know about a user.
+    user_info['first_name'] = first
+    user_info['last_name'] = last    
+    return user_info
 
-
+user_profile = build_profile('albert', 'einstein', location='princeton', field='physics')
+print(user_profile)
 
 
 
