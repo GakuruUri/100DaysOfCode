e696457b390f48a0f261fe4c5e4f458bff8b6ca2
GakuruUri <urigakuru@icloud.com>
2024-02-13 16:28:35 +0300

    Day 49

--- pythonCrushCourse/python_work/Part 1/uri.py
+++ pythonCrushCourse/python_work/Part 1/uri.py
@@ -418,11 +418,43 @@ my_tesla.battery.get_range()
 my_tesla.battery.upgrade_battery()
 my_tesla.battery.get_range()
 '''
-def dis(price, discount):
-	disc = price * (discount/100)
-	return price - disc
-	
-print(dis(593, 61))
+"""A class that can be used to represent a car. """
+
+class Car:
+    # A simple attemp to represent a car
+    def __init__(self, make, model, year):
+        # Initialize attributes to describe a car.
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+
+    def get_descriptive_name(self):
+        # Return a neatly formatted descriptive name.
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
+
+    def read_odometer(self):
+        # Print a statement showing a car's mileage
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+
+    def update_odometer(self, mileage):
+        #Set odometer reading to a given vale
+        # Reject the change if it attempts to roll the odometer back.
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+
+        else:
+             print(f"You can't roll back an odometer!")
+
+
+    def increment_odometer(self, miles):
+        # Add the amount to the odometer reading
+        self.odometer_reading += miles
+
 
 
 
