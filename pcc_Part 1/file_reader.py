93a73ae444287466773821ae964423ffd38222f7
GakuruUri <urigakuru@icloud.com>
2024-02-17 22:20:23 +0300

    Day 51 again

---
+++ pcc_Part 1/file_reader.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+path = Path('pi_digits.txt')
+contents = path.read_text()
+
+print(contents)
+
+
+print("\nStrip/remove whitespace from the contents")
+
+from pathlib import Path
+
+path = Path('pi_digits.txt')
+contents = path.read_text()
+contents = contents.rstrip()
+
+print(contents)
+
+
+print("\nApplying rstrip() after we call read_text()")
+from pathlib import Path
+
+path = Path('pi_digits.txt')
+contents = path.read_text().rstrip()
+
+print(contents)
+
+
+
